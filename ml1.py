9185905e3936351b821538365c534dcb867b2507
Malekze99 <154664666+Malekze99@users.noreply.github.com>
2025-07-08 07:29:10 +0100

    Update ml1.py

--- ml1.py
+++ ml1.py
@@ -725,7 +725,7 @@ def train_and_evaluate_model(data: pd.DataFrame) -> Tuple[Any, Dict[str, Any]]:
         sample_weight=sample_weights,
         eval_set=[(X_test_scaled_df, y_test)],
         eval_metric='binary_logloss',
-        callbacks=[early_stopping(stopping_rounds=50)],
+        early_stopping=50,
         verbose=10
     )
     
